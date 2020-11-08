f43abd8a602b8b6ad272b59fcdfeafb6f7f78a7f
RealmHost002 <tukow@yandex.ru>
2020-11-08 19:15:51 +0300

    trees

--- Camera.gd
+++ Camera.gd
@@ -22,7 +22,7 @@ func _input(event):
 #		print(result)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass # Replace with function body.
 
 
